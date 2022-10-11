db2f3a9e78b39cdaeb28560b3f59e3bbdc6dc782
LeoS <seryleo@orange.fr>
2022-10-11 17:00:58 +0200

    Project: Add base files

---
+++ Overcooked/Assets/Scripts/PlayerDeplacement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerDeplacement : MonoBehaviour
+{
+    public CharacterController characterController;
+
+    [SerializeField] private float Speed = 10f;
+    [SerializeField] private float turnSmooth = 1f;
+    float turnSmoothVelocity;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) *  Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmooth, turnSmoothVelocity);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            characterController.Move(direction * Speed * Time.deltaTime);
+        }
+    }
+}
